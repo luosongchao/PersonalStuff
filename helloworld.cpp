a550dcdbdeae372348253243695267e71e8d007e
luosongchao <814010510@qq.com>
2013-07-04 15:36:30 +0800

    delete helloworld.cpp test.cpp

--- helloworld.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	int value=12;
-	cout<<value<<endl;  
-	printf("who am i?");
-}
-
--- test.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int mian()
-{
-  cout<<"it't wonderful !"<<endl;
-}
